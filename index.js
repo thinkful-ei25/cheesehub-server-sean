0b1fe7d23f8650ed658fea4cbb890ac3c8059284
Sean Nealon <signalflowsean@gmail.com>
2018-11-16 11:20:51 -0500

    get api/cheeses/ endpoint returns cheeses

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ app.use(
   })
 );
 
+app.get('/api/cheeses', (req, res) => { 
+  const cheeses =  [
+    'Bath Blue',
+    'Barkham Blue',
+    'Buxton Blue',
+    'Cheshire Blue',
+    'Devon Blue',
+    'Dorset Blue Vinney',
+    'Dovedale',
+    'Exmoor Blue',
+    'Harbourne Blue',
+    'Lanark Blue',
+    'Lymeswold',
+    'Oxford Blue',
+    'Shropshire Blue',
+    'Stichelton',
+    'Stilton',
+    'Blue Wensleydale',
+    'Yorkshire Blue'
+  ];
+  return res.json(cheeses); 
+
+}); 
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
